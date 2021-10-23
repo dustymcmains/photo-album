1773e660b51baecb492b7f04024e9400290e1523
McMains, Dustin R <dustin-mcmains@uiowa.edu>
2021-10-23 14:10:54 -0500

    functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'photo-album';
+export class AppComponent implements OnInit {
+  title = 'photos';
+
+  public searchText = '';
+
+  public photos: Array<any> = [];
+
+  constructor(
+    private http: HttpClient
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.setList();
+  }
+
+  public getList(): Observable<any> {
+    return this.http.get<any>('https://jsonplaceholder.typicode.com/photos');
+  }
+
+  public setList(): Observable<any> | void {
+    this.getList().subscribe(result => {
+      console.log(result);
+    });
+  }
+
+  public getResults(): Observable<any> {
+    return this.http.get<any>('https://jsonplaceholder.typicode.com/photos' + '?albumId=' + this.searchText);
+  }
+
+  public setResults(): Observable<any> | void {
+    this.getResults().subscribe(result => {
+      console.log(result);
+      const array = [];
+      for (let i = 0; i < result.length; i++) {
+        this.photos = result[i];
+        array.push(this.photos);
+        console.log(this.photos);
+        this.photos = array;
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
